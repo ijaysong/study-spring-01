7ccee6ba343cb8d279088a29dd7424f2b5a53445
Eunji Song <ijaysong0811@gmail.com>
2020-09-25 23:06:02 +0900

    DAO 기초

---
+++ src/main/java/com/project/toby/dao/UserDao.java
@@ -0,0 +1,62 @@
+package com.project.toby.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.project.toby.domain.User;
+
+public class UserDao {
+	
+	/**
+	 * 사용자 정보 등록
+	 * @param user 사용자 정보
+	 * @throws Exception
+	 */
+	public void add(User user) throws Exception {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection c = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/dev_db?serverTimezone=UTC&characterEncoding=UTF-8&useSSL=false"
+				, "ejsong", "1qaz2wsx");
+		
+		PreparedStatement ps = c.prepareStatement(
+				"INSERT INTO USER (ID, NAME, PASSWORD) VALUES (?, ?, ?)");
+		ps.setString(1, user.getId());
+		ps.setString(2, user.getName());
+		ps.setString(3, user.getPassword());
+		
+		ps.executeUpdate();
+		
+		ps.close();
+		c.close();
+	}
+	
+	/**
+	 * 사용자 정보 취득
+	 * @param id 아이디
+	 * @return
+	 * @throws Exception
+	 */
+	public User get(String id) throws Exception {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection c = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/dev_db?serverTimezone=UTC&characterEncoding=UTF-8&useSSL=false"
+				, "ejsong", "1qaz2wsx");
+		
+		PreparedStatement ps = c.prepareStatement(
+				"SELECT * FROM USER WHERE id = ?");
+		ps.setString(1, id);
+		
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+		User user = new User();
+		user.setId(rs.getString("id"));
+		user.setName(rs.getString("name"));
+		user.setPassword(rs.getString("password"));
+		
+		rs.close();
+		ps.close();
+		c.close();
+		
+		return user;
+	}
+}
