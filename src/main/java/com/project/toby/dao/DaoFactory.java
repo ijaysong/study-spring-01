147bd9b78bdcec9e7589f0b9f914d06fa56eddc3
Eunji Song <ijaysong0811@gmail.com>
2020-10-04 23:14:56 +0900

    스프링의 IoC

--- src/main/java/com/project/toby/dao/DaoFactory.java
+++ src/main/java/com/project/toby/dao/DaoFactory.java
@@ -1,11 +1,19 @@
 package com.project.toby.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * 애플리케이션의 오브젝트들을 구성하고 그 관계를 정의하는 역할 수행
  *
  */
+
+// 애플리케이션 컨텍스트 또는 빈 팩토리가 사용할 설정정보라는 표시
+@Configuration
 public class DaoFactory {
 	
+	// 오브젝트 생성을 담당하는 IoC용 메소드라는 표시
+	@Bean
 	public UserDao userDao() {
 		return new UserDao(connectionMaker());
 	}
@@ -23,6 +31,7 @@ public class DaoFactory {
 	/*
 	 * DB 연결
 	 */
+	@Bean
 	public ConnectionMaker connectionMaker() {
 		return new DConnectionMaker(); // 분리해서 중복을 제거한 ConnectionMaker 타입 오브젝트 생성 코드
 	}
--- src/main/java/com/project/toby/test/UserDaoTest.java
+++ src/main/java/com/project/toby/test/UserDaoTest.java
@@ -2,6 +2,9 @@ package com.project.toby.test;
 
 import java.sql.SQLException;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import com.project.toby.dao.DaoFactory;
 import com.project.toby.dao.UserDao;
 import com.project.toby.domain.User;
@@ -9,7 +12,9 @@ import com.project.toby.domain.User;
 public class UserDaoTest {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		UserDao dao = new DaoFactory().userDao();
+		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		UserDao dao = context.getBean("userDao", UserDao.class);
+		//UserDao dao = new DaoFactory().userDao();
 		// ConnectionMaker connectionMaker = new DConnectionMaker(); 
 		// ConnectionMaker connectionMaker = new NConnectionMaker(); 
 		
