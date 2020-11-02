c616ce94a04b397789a72d0b825226e82224217a
Eunji Song <ijaysong0811@gmail.com>
2020-11-02 17:05:40 +0900

    스프링 빈 팩토리 활용

--- src/main/java/com/project/toby/dao/DaoFactory.java
+++ src/main/java/com/project/toby/dao/DaoFactory.java
@@ -25,11 +25,17 @@ public class DaoFactory {
 	*/
 	
 	// 팩토리의 메소드는 UserDao 타입의 오브젝트를 어떻게 만들고, 어떻게 준비시킬지를 결정한다.
+	/*
 	public UserDao userDao() {
 		ConnectionMaker connectionMaker = new DConnectionMaker();
 		UserDao userDao = new UserDao(connectionMaker);
 
 		return userDao;
+	}*/
+	
+	@Bean
+	public UserDao userDao() {
+		return new UserDao(connectionMaker());
 	}
 	
 	/*
