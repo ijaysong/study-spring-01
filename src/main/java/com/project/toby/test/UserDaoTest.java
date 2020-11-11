ebdaab5d74e666f66814d37e8c46e2760637d3f7
Eunji Song <ijaysong0811@gmail.com>
2020-11-11 23:43:37 +0900

    JUnit 프레임워크에서 동작할 수 있는 테스트 메소드 추가

--- src/main/java/com/project/toby/test/UserDaoTest.java
+++ src/main/java/com/project/toby/test/UserDaoTest.java
@@ -2,6 +2,7 @@ package com.project.toby.test;
 
 import java.sql.SQLException;
 
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
@@ -28,6 +29,33 @@ public class UserDaoTest {
 		
 		//UserDao dao = new UserDao(connectionMaker); 
 		//UserDao dao = new DaoFactory().userDao();
+		
+		User user = new User();
+		user.setId("hongkd");
+		user.setName("홍길동");
+		user.setPassword("1234");
+		
+		dao.add(user);
+		
+		User user2 = dao.get(user.getId());
+		System.out.println(user2.getName());
+		System.out.println(user2.getPassword());
+		
+		if (!user.getName().equals(user2.getName())) {
+			System.out.println("테스트 실패 (name)");
+		}
+		else if (!user.getPassword().equals(user2.getPassword())) {
+			System.out.println("테스트 실패 (password)");
+		}
+		else {
+			System.out.println("조회 테스트 성공");
+		}
+	}
+	
+	@Test
+	public void addAndGet() throws SQLException, ClassNotFoundException {
+		ApplicationContext context = new GenericXmlApplicationContext("com/project/toby/application.xml");
+		UserDao dao = context.getBean("userDao", UserDao.class);
 		
 		User user = new User();
 		user.setId("hongkd");
