64fd096264d4e604752a0734f042adabcccf660b
Eunji Song <ijaysong0811@gmail.com>
2020-11-17 23:47:01 +0900

    출력 메시지 변경

--- src/main/java/com/project/toby/test/UserDaoTest.java
+++ src/main/java/com/project/toby/test/UserDaoTest.java
@@ -63,10 +63,10 @@ public class UserDaoTest {
 		user.setPassword("5678");
 		
 		dao.add(user);
+		System.out.println(user.getId() + "등록 성공");
 		
 		User user2 = dao.get(user.getId());
-		System.out.println(user2.getName());
-		System.out.println(user2.getPassword());
+		System.out.println(user2.getId() + "조회 성공");
 		
 		if (!user.getName().equals(user2.getName())) {
 			System.out.println("테스트 실패 (name)");
